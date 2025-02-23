eff24c0651402556bd2c099a1a49f8dd61c219cc
Udeshi Edirisinghe <ar112949@fhss.sjp.ac.lk>
2025-02-23 14:45:42 +0530

    Started Backend.
    Started to Create backend of the registration and login page.

---
+++ SmartBus/SmartBus/SmartBus/DBconnection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace SmartBus
+{
+    class DBconnection
+    {
+        static readonly string server = "localhost";
+        static readonly string database = "Smartbus";
+        static readonly string user = "root";
+        static readonly string password = "";
+        public static string connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "USER=" + user + ";" + "PASSWORD=" + password + ";";
+        public MySqlConnection mysqlconnection = new MySqlConnection(connectionString);
+        public bool connect_db()
+        {
+            try
+            {
+                mysqlconnection.Open();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        public bool close_db()
+        {
+            try
+            {
+                mysqlconnection.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+        }
+
+    }
+}
---
+++ SmartBus/SmartBus/SmartBus/Form1.cs
@@ -0,0 +1,97 @@
+using MySql.Data.MySqlClient;
+
+namespace SmartBus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            // Get username and password from textboxes
+            string username = txtUserName.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            // Basic validation 
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Username and password cannot be empty!");
+
+                return;
+
+            }
+            else if (password.Length < 8 || password.Length > 12)
+            {
+                MessageBox.Show($"Password must be at least {8} characters long.", "Short Password", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            else
+            {
+                MessageBox.Show("Please wait a moment!", "Login In ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            // database
+            // database
+            try
+            {
+                var db = new DBconnection();
+                if (db.connect_db())
+                {
+                    string query = "SELECT * FROM users WHERE UserName = @username AND Password = @password";
+                    MySqlCommand cmd = new MySqlCommand(query, db.mysqlconnection);
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@password", password);
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string userType = reader["UserType"].ToString();
+                            MessageBox.Show("Login Successful", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                            if (userType == "Admin")
+                            {
+                                Admin admin = new Admin();
+                                admin.Show();
+                                this.Hide();
+                            }
+                            else if (userType == "User")
+                            {
+                                User user = new User();
+                                user.Show();
+                                this.Hide();
+                            }
+
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid username or password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        ;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Registration reg = new Registration();
+            reg.Show();
+            
+        }
+    }
+}
