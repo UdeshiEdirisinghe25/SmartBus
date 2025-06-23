34a34a0880698a90d41f3ec5755d8484e193e431
UdeshiEdirisinghe25 <ar112949@fhss.sjp.ac.lk>
2025-06-23 12:06:51 +0530

    Created Admin Dashboard

---
+++ SmartBus/SmartBus/SmartBus/SmartBus/Admin.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace SmartBus
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            panel1 = new Panel();
+            panel4 = new Panel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            btnRefresh = new Button();
+            dataGridView1 = new DataGridView();
+            Edit = new DataGridViewButtonColumn();
+            Delete = new DataGridViewButtonColumn();
+            linklblAddBus = new LinkLabel();
+            panel3 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            btnLogout = new Button();
+            btnBook = new Button();
+            label2 = new Label();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(panel4);
+            panel1.Controls.Add(btnRefresh);
+            panel1.Controls.Add(dataGridView1);
+            panel1.Controls.Add(linklblAddBus);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(1, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1624, 933);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.WhiteSmoke;
+            panel4.Controls.Add(tableLayoutPanel1);
+            panel4.Location = new Point(458, 113);
+            panel4.Margin = new Padding(3, 4, 3, 4);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(1138, 320);
+            panel4.TabIndex = 25;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.Location = new Point(22, 16);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(1090, 280);
+            tableLayoutPanel1.TabIndex = 0;
+            tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.BackColor = Color.White;
+            btnRefresh.FlatStyle = FlatStyle.Flat;
+            btnRefresh.Font = new Font("Malgun Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnRefresh.ForeColor = Color.DarkSlateBlue;
+            btnRefresh.Location = new Point(1490, 516);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Size = new Size(106, 40);
+            btnRefresh.TabIndex = 24;
+            btnRefresh.Text = "Refresh";
+            btnRefresh.UseVisualStyleBackColor = false;
+            btnRefresh.Click += btnRefresh_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.BackgroundColor = Color.WhiteSmoke;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Edit, Delete });
+            dataGridView1.Location = new Point(458, 579);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1138, 267);
+            dataGridView1.TabIndex = 2;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Edit
+            // 
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(64, 64, 64);
+            dataGridViewCellStyle1.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle1.ForeColor = Color.Black;
+            dataGridViewCellStyle1.NullValue = "Edit";
+            dataGridViewCellStyle1.SelectionBackColor = Color.Silver;
+            dataGridViewCellStyle1.SelectionForeColor = Color.FromArgb(64, 64, 64);
+            Edit.DefaultCellStyle = dataGridViewCellStyle1;
+            Edit.HeaderText = "Edit";
+            Edit.MinimumWidth = 6;
+            Edit.Name = "Edit";
+            Edit.ReadOnly = true;
+            Edit.Resizable = DataGridViewTriState.True;
+            Edit.SortMode = DataGridViewColumnSortMode.Automatic;
+            Edit.Width = 125;
+            // 
+            // Delete
+            // 
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = Color.FromArgb(64, 64, 64);
+            dataGridViewCellStyle2.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle2.ForeColor = Color.FromArgb(64, 64, 64);
+            dataGridViewCellStyle2.NullValue = "Delete";
+            dataGridViewCellStyle2.SelectionBackColor = Color.Silver;
+            dataGridViewCellStyle2.SelectionForeColor = Color.FromArgb(64, 64, 64);
+            Delete.DefaultCellStyle = dataGridViewCellStyle2;
+            Delete.HeaderText = "Delete";
+            Delete.MinimumWidth = 6;
+            Delete.Name = "Delete";
+            Delete.ReadOnly = true;
+            Delete.Resizable = DataGridViewTriState.True;
+            Delete.SortMode = DataGridViewColumnSortMode.Automatic;
+            Delete.Width = 125;
+            // 
+            // linklblAddBus
+            // 
+            linklblAddBus.AutoSize = true;
+            linklblAddBus.BackColor = Color.Transparent;
+            linklblAddBus.Font = new Font("Malgun Gothic", 18F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
+            linklblAddBus.ForeColor = Color.SteelBlue;
+            linklblAddBus.LinkColor = Color.SteelBlue;
+            linklblAddBus.Location = new Point(458, 516);
+            linklblAddBus.Name = "linklblAddBus";
+            linklblAddBus.Size = new Size(139, 41);
+            linklblAddBus.TabIndex = 0;
+            linklblAddBus.TabStop = true;
+            linklblAddBus.Text = "Add Bus";
+            linklblAddBus.LinkClicked += linklblAddBus_LinkClicked;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.LightSteelBlue;
+            panel3.Controls.Add(pictureBox1);
+            panel3.Location = new Point(-3, 87);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(432, 792);
+            panel3.TabIndex = 1;
+            panel3.Paint += panel3_Paint;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.png;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(13, 181);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(414, 420);
+            pictureBox1.TabIndex = 25;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.SteelBlue;
+            panel2.Controls.Add(btnLogout);
+            panel2.Controls.Add(btnBook);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Location = new Point(-1, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1903, 87);
+            panel2.TabIndex = 0;
+            panel2.Paint += panel2_Paint;
+            // 
+            // btnLogout
+            // 
+            btnLogout.BackColor = Color.White;
+            btnLogout.FlatStyle = FlatStyle.Flat;
+            btnLogout.Font = new Font("Malgun Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLogout.ForeColor = Color.DarkSlateBlue;
+            btnLogout.Location = new Point(1515, 19);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(91, 47);
+            btnLogout.TabIndex = 25;
+            btnLogout.Text = "Logout";
+            btnLogout.UseVisualStyleBackColor = false;
+            btnLogout.Click += btnLogout_Click_1;
+            // 
+            // btnBook
+            // 
+            btnBook.BackColor = Color.SlateGray;
+            btnBook.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBook.ForeColor = SystemColors.ActiveCaptionText;
+            btnBook.Location = new Point(1663, 16);
+            btnBook.Name = "btnBook";
+            btnBook.Size = new Size(106, 40);
+            btnBook.TabIndex = 23;
+            btnBook.Text = "Logout";
+            btnBook.UseVisualStyleBackColor = false;
+            btnBook.Click += btnBook_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Britannic Bold", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(758, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(130, 44);
+            label2.TabIndex = 2;
+            label2.Text = "Admin";
+            label2.Click += label2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Script MT Bold", 24F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(3, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(186, 48);
+            label1.TabIndex = 1;
+            label1.Text = "SmartBus";
+            // 
+            // Admin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1622, 881);
+            Controls.Add(panel1);
+            Name = "Admin";
+            Text = "Admin";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+        private Label label2;
+        private Panel panel3;
+        private LinkLabel linklblAddBus;
+        private DataGridView dataGridView1;
+        private Button btnBook;
+        private DataGridViewButtonColumn Edit;
+        private DataGridViewButtonColumn Delete;
+        private Button btnRefresh;
+        private Button btnLogout;
+        private PictureBox pictureBox1;
+        private Panel panel4;
+        private TableLayoutPanel tableLayoutPanel1;
+    }
+}
---
+++ SmartBus/SmartBus/SmartBus/SmartBus/Admin.cs
@@ -0,0 +1,263 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartBus
+{
+    public partial class Admin : Form
+    {
+        public Admin()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Admin_Load);
+        }
+        private void Admin_Load(object sender, EventArgs e)
+        {
+            loaddata();
+            LoadDashboard();
+        }
+        // admin label
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //load bus details
+        public void loaddata()
+        {
+            try
+            {
+                var DBconnection = new DBconnection();
+                if (DBconnection.connect_db())
+                {
+                    string query = "select BusID, BusNumber, Route, DriverNTC, ConductorNTC, Price, TravelTime, TravelDistance, DepatureTime from busdetails";
+                    MySqlCommand cmd = new MySqlCommand(query);
+                    cmd.Connection = DBconnection.mysqlconnection;
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    BindingSource bsource = new BindingSource();
+                    bsource.DataSource = dt;
+
+                    dataGridView1.DataSource = bsource;
+
+                    DBconnection.close_db();
+
+                }
+                else
+                {
+                    MessageBox.Show("Database connection Failed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        //logout
+        private void btnBook_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Hide();
+        }
+
+        // bus details
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            loaddata();
+        }
+
+        private void linklblAddBus_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            AddBus addbus = new AddBus();
+            addbus.Show();
+        }
+
+        // edit delete bus details
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                // Get the bus ID from the 3rd column (index 2)
+                object thirdCellValue = dataGridView1.Rows[e.RowIndex].Cells[2].Value;
+
+                if (thirdCellValue != null && int.TryParse(thirdCellValue.ToString(), out int busIdCellValue))
+                {
+                    if (e.ColumnIndex == 0)
+                    {
+                        // Edit button clicked
+                        EditBus edit = new EditBus(busIdCellValue);
+                        edit.Show();
+                    }
+                    else if (e.ColumnIndex == 1)
+                    {
+                        // Delete button clicked
+                        DialogResult result = MessageBox.Show("Are you sure you want to delete this record?",
+                                                              "Confirm Delete",
+                                                              MessageBoxButtons.YesNo,
+                                                              MessageBoxIcon.Warning);
+
+                        if (result == DialogResult.Yes)
+                        {
+                            var db = new DBconnection();
+                            if (db.connect_db())
+                            {
+                                string query = "DELETE FROM busdetails WHERE BusID = @BusID";
+                                MySqlCommand cmd = new MySqlCommand(query, db.mysqlconnection);
+                                cmd.Parameters.AddWithValue("@BusID", busIdCellValue);
+
+                                int rowsAffected = cmd.ExecuteNonQuery();
+
+                                if (rowsAffected > 0)
+                                {
+                                    MessageBox.Show("Record deleted successfully.");
+                                    // reload the DataGridView
+                                    loaddata();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Failed to delete record.");
+                                }
+
+                                db.close_db();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No valid data! Please select a bus");
+                }
+            }
+        }
+
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+           // loaddata();
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnLogout_Click_1(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            form.Show();
+            this.Hide();
+
+        }
+
+
+        // count tickets and revenue
+        private int GetCount(string query)
+        {
+            int count = 0;
+            var db = new DBconnection();
+            if (db.connect_db())
+            {
+                MySqlCommand cmd = new MySqlCommand(query, db.mysqlconnection);
+                count = Convert.ToInt32(cmd.ExecuteScalar());
+                db.close_db();
+            }
+            return count;
+        }
+
+        // get revenue
+        private decimal GetRevenue(string query)
+        {
+            decimal revenue = 0;
+            var db = new DBconnection();
+            if (db.connect_db())
+            {
+                MySqlCommand cmd = new MySqlCommand(query, db.mysqlconnection);
+                object result = cmd.ExecuteScalar();
+                if (result != DBNull.Value)
+                {
+                    revenue = Convert.ToDecimal(result);
+                }
+                db.close_db();
+            }
+            return revenue;
+        }
+
+        // table
+        private Panel CreateMetricPanel(string title, string value, Color backColor)
+        {
+            Panel panel = new Panel
+            {
+                Dock = DockStyle.Fill,
+                BackColor = backColor,
+                Margin = new Padding(10)
+            };
+
+            Label lblTitle = new Label
+            {
+                Text = title,
+                Dock = DockStyle.Top,
+                Font = new Font("Segoe UI", 12, FontStyle.Bold),
+                TextAlign = ContentAlignment.MiddleCenter,
+                Height = 30
+            };
+
+            Label lblValue = new Label
+            {
+                Text = value,
+                Dock = DockStyle.Fill,
+                Font = new Font("Segoe UI", 22, FontStyle.Bold),
+                TextAlign = ContentAlignment.MiddleCenter,
+                ForeColor = Color.DarkBlue
+            };
+
+            panel.Controls.Add(lblValue);
+            panel.Controls.Add(lblTitle);
+            return panel;
+        }
+
+        // load dashboard
+        private void LoadDashboard()
+        {
+            int todayBookings = GetCount("SELECT COUNT(*) FROM booking WHERE Date = CURDATE()");
+            int weekBookings = GetCount("SELECT COUNT(*) FROM booking WHERE WEEK(Date, 1) = WEEK(CURDATE(), 1) AND YEAR(Date) = YEAR(CURDATE())");
+            int monthBookings = GetCount("SELECT COUNT(*) FROM booking WHERE MONTH(Date) = MONTH(CURDATE()) AND YEAR(Date) = YEAR(CURDATE())");
+
+            decimal todayRevenue = GetRevenue("SELECT SUM(Price) FROM booking WHERE Date = CURDATE()");
+            decimal weekRevenue = GetRevenue("SELECT SUM(Price) FROM booking WHERE WEEK(Date, 1) = WEEK(CURDATE(), 1) AND YEAR(Date) = YEAR(CURDATE())");
+            decimal monthRevenue = GetRevenue("SELECT SUM(Price) FROM booking WHERE MONTH(Date) = MONTH(CURDATE()) AND YEAR(Date) = YEAR(CURDATE())");
+
+            tableLayoutPanel1.Controls.Clear();
+
+            // Row 0: Ticket Counts
+            tableLayoutPanel1.Controls.Add(CreateMetricPanel("Tickets Today", todayBookings.ToString(), Color.LightBlue), 0, 0);
+            tableLayoutPanel1.Controls.Add(CreateMetricPanel("Tickets This Week", weekBookings.ToString(), Color.LightGreen), 1, 0);
+            tableLayoutPanel1.Controls.Add(CreateMetricPanel("Tickets This Month", monthBookings.ToString(), Color.LightYellow), 2, 0);
+
+            // Row 1: Revenues
+            tableLayoutPanel1.Controls.Add(CreateMetricPanel("Revenue Today", $"Rs. {todayRevenue:N2}", Color.MistyRose), 0, 1);
+            tableLayoutPanel1.Controls.Add(CreateMetricPanel("Revenue This Week", $"Rs. {weekRevenue:N2}", Color.LightSkyBlue), 1, 1);
+            tableLayoutPanel1.Controls.Add(CreateMetricPanel("Revenue This Month", $"Rs. {monthRevenue:N2}", Color.LightSalmon), 2, 1);
+        }
+
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+           // LoadDashboard();
+        }
+    }
+}
